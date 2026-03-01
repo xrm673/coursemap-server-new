8c1cdf512c0cce10365227cf0de07996d8d3b26d
Raymond Xu <rx67@cornell.edu>
2026-03-01 11:25:57 -0500

    构建单元测试架构

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+};
+
+export default config;
