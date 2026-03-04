fd42152de2afca012d35e1c740076c93a3b63acd
Raymond Xu <rx67@cornell.edu>
2026-03-03 21:40:59 -0500

    bugfix: 解决CORS跨域问题

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
